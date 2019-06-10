80b13f1ac40343be8d92465e1d4298d434cfe530
HARSHIT PITHI <harshitpithi1967@gmail.com>
2019-06-10 20:58:01 +0530

    Add files via upload

---
+++ OutputTransformation.java
@@ -0,0 +1,21 @@
+package Hackathon;
+
+public class OutputTransformation {
+	private String columnDataType;
+	public String getColumnDataType() {
+		return columnDataType;
+	}
+	public void setColumnDataType(String columnDataType) {
+		this.columnDataType = columnDataType;
+	}
+	public String getTransformation() {
+		return transformation;
+	}
+	public void setTransformation(String transformation) {
+		this.transformation = transformation;
+	}
+	private String transformation;
+	
+	
+		
+}
